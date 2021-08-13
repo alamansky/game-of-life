1556ccb68630d7848e355488b0f6bb36b3774b56
Andrew Lamansky <andrewlamansky@gmail.com>
2021-08-13 10:02:21 -0400

    update remote repository

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,24 +2,130 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Child from './../Child/Child.js';
+import Grid from './../Grid/Grid.js';
+import Box from './../Box/Box.js';
+import Buttons from './../Buttons/Buttons';
 
 import './App.css';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.speed = 1000;
+		this.rows = 30;
+		this.cols = 50;
+
 		this.state = {
-			init: 'React here!'
+			generation: 0,
+			gridFull: Array(this.rows).fill(Array(this.cols).fill(false))
 		};
+	}
+
+	selectBox = (row, col) => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		gridCopy[row][col] = !gridCopy[row][col];
+		this.setState({ gridFull: gridCopy });
+	}
+
+	seed = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				if (Math.floor(Math.random() * 3) === 1) {
+					gridCopy[i][j] = true;
+				}
+			}
+		}
+		this.setState({ gridFull: gridCopy });
+	}
+
+	playButton = () => {
+		clearInterval(this.intervalId);
+		this.intervalId = setInterval(this.play, this.speed);
+	}
+
+	pauseButton = () => {
+		clearInterval(this.intervalId);
+	}
+
+	slow = () => {
+		this.speed = 2000;
+		this.playButton();
+	}
+
+	fast = () => {
+		this.speed = 100;
+		this.playButton();
+	}
+
+	clear = () => {
+		var grid = Array(this.rows).fill(Array(this.cols).fill(false));
+		this.setState({
+			gridFull: grid,
+			generation: 0
+		});
+	}
+
+	play = () => {
+		let g = this.state.gridFull;
+		let g2 = arrayClone(this.state.gridFull);
+
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				let count = 0;
+				if (i > 0) if (g[i - 1][j]) count++;
+				if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+				if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+				if (j < this.cols - 1) if (g[i][j + 1]) count++;
+				if (j > 0) if (g[i][j - 1]) count++;
+				if (i < this.rows - 1) if (g[i + 1][j]) count++;
+				if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+				if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+				if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+				if (!g[i][j] && count === 3) g2[i][j] = true;
+			}
+		}
+
+		this.setState({
+			gridFull: g2,
+			generation: this.state.generation + 1
+		});
+	}
+
+	componentDidMount() {
+		this.seed();
+		this.playButton();
 	}
 
 	render() {
 		return (
 			<div>
+				<h1>The Game of Life</h1>
+				<Buttons
+					playButton={this.playButton}
+					pauseButton={this.pauseButton}
+					slow={this.slow}
+					fast={this.fast}
+					clear={this.clear}
+					seed={this.seed}
+					gridSize={this.gridSize}
+				/>
+				<Grid
+					gridFull={this.state.gridFull}
+					cols={this.cols}
+					rows={this.rows}
+					selectBox={this.selectBox}
+				/>
+				<h2>Generatrions: {this.state.generation}</h2>
 				<Child props={this.state.init} />
 			</div>
 		);
 	}
+}
+
+function arrayClone(arr) {
+	return JSON.parse(JSON.stringify(arr));
 }
 
 export default App;
